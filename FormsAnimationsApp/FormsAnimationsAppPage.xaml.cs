1d98ae3b1c4a54eca5930d62b67c4fedba027fce
Abhishek Kumar <abhishabd1@outlook.com>
2018-03-20 16:51:47 +0530

    * UserPrefs.xml: Initial Commit
    	* Main.cs:
    	* Info.plist:
    	* AppDelegate.cs:
    	* packages.config:
    	* MainActivity.cs:
    	* packages.config:
    	* FormsAnimationsApp.sln:
    	* Entitlements.plist:
    	* monkey.png:
    	* LaunchScreen.storyboard:
    	* App.xaml:
    	* AboutAssets.txt:
    	* App.xaml.cs:
    	* AssemblyInfo.cs:
    	* styles.xml:
    	* Contents.json:
    	* icon.png:
    	* FormsAnimationsApp.iOS.csproj:
    	* packages.config:
    	* AboutResources.txt:
    	* Tabbar.axml:
    	* Toolbar.axml:
    	* monkey.png:
    	* Resource.designer.cs:
    	* AndroidManifest.xml:
    	* FormsAnimationsApp.Droid.csproj:
    	* icon.png:
    	* icon.png:
    	* icon.png:
    	* FormsAnimationsApp.csproj:
    	* AssemblyInfo.cs:
    	* FormsAnimationsAppPage.xaml:
    	* FormsAnimationsAppPage.xaml.cs:
    	* Contents.json:

---
+++ FormsAnimationsApp/FormsAnimationsAppPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Xamarin.Forms;
+
+namespace FormsAnimationsApp
+{
+    public partial class FormsAnimationsAppPage : ContentPage
+    {
+        public FormsAnimationsAppPage()
+        {
+            InitializeComponent();
+            lblGret.IsVisible = true;
+
+            //Translate effect
+            lblGret.TranslateTo(100, 0, 1000, Easing.BounceOut);
+
+            //Scale
+            xamImage.ScaleTo(1.5, 2000, Easing.SpringIn);
+
+            //Rotate
+            lblForms.RotateTo(360,1000);
+
+            //Fade
+            lblFade.FadeTo(0.2, 2000, Easing.SpringOut);
+
+
+
+        }
+
+
+
+
+    }
+}
